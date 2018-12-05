2160c3aa10c9c7fe3a1216417288c0c93de01d66
pickledcheese <shaneosgood1@gmail.com>
2018-12-04 18:25:23 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,56 +1,24 @@
-/* 
- * File:   main.cpp
- * Author: Mahir Rahman & Shane Osgood
- * Created on November 2018
- * Purpose: CIS 7 Project: Vigenère cipher
- */
-//System Libraries
-#include<bits/stdc++.h> 
 #include<stdio.h>
 #include<string.h>
 #include<iostream>
 #include<string>
 #include<cstring>
-#include<fstream>
 
-using namespace std;//namespace I/O stream library created
+using namespace std; 
 
-//User Libraries
-
-//Global Constants
-//Math, Physics, Science, Conversions, 2-D Array Columns
-
-//Function Prototypes
-
-
-//Execution Begins Here!
-int main(int argc, char** argv) {
-    //Declare Variables
-    ofstream filelog;
-
+int main(){
     string mssg;
     string keyy;
-    
-    string convert;
-    //Initial Variables
-    
-    //Map/Process Inputs to Outputs
-    cout<<"Enter message to encrypt(MESSAGE MUST BE SINGLE STRING WITH NO SPACES OR SYMBOLS): ";
-    getline(cin,mssg);    
-    //Transforms lowercase letters to uppercase
-    transform(mssg.begin(), mssg.end(), mssg.begin(), ::toupper); 
 
-    
-//Converts string to char
+    cout<<"Enter message to encrypt: ";
+    cin>>mssg;
+
     char msg[mssg.size()+1];
     strcpy(msg, mssg.c_str());
 
     cout<<"Please input key for code: ";
-    getline(cin,keyy);    
-    //Transforms lowercase letters to uppercase
-    transform(keyy.begin(), keyy.end(), keyy.begin(), ::toupper); 
+    cin>>keyy;
 
-    //Converts string to char
     char key[keyy.size()+1];
     strcpy(key, keyy.c_str());
 
@@ -60,47 +28,35 @@ int main(int argc, char** argv) {
     char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
 
     
-    //Generating new key
+
+    
+    //generating new key
     for(i = 0, j = 0; i < msgLen; ++i, ++j){
         if(j == keyLen)
             j = 0;
  
         newKey[i] = key[j];
     }
-        
+ 
     newKey[i] = '\0';
  
-    //Encryption
+    //encryption
     for(i = 0; i < msgLen; ++i)
         encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
-    
+ 
     encryptedMsg[i] = '\0';
  
-    //Decryption
+    //decryption
     for(i = 0; i < msgLen; ++i)
         decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
-    
+ 
     decryptedMsg[i] = '\0';
-    
-    //Display Outputs
-    printf("\nOriginal Message: %s", msg);
+ 
+    printf("Original Message: %s", msg);
     printf("\nKey: %s", key);
     printf("\nNew Generated Key: %s", newKey);
     printf("\nEncrypted Message: %s", encryptedMsg);
     printf("\nDecrypted Message: %s", decryptedMsg);
-    
-    //Open File
-    filelog.open("filelog.txt", fstream::app);
-    filelog<<"\nOriginal Message: "<<msg;
-    filelog<<"\nKey: "<<key;
-    filelog<<"\nNew Generated Key: "<<newKey;
-    filelog<<"\nEncrypted Message: "<<encryptedMsg;
-    filelog<<"\nDecrypted Message: "<<decryptedMsg;
-    filelog<<"\n***************************************";
-    //Close File
-    filelog.close();
-    
-    //Exit program!
+ 
     return 0;
 }
-
